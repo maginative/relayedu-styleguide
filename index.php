9ed367136774f9bd9a50e92f811a239cdefeb91a
Gustavo Ramirez Fallas <gustavorf90@gmail.com>
2020-11-18 00:17:07 -0600

    Added basics styles

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en" class="no-js">
 <head>
 <meta charset="utf-8">
-  <title>Wowway! Style Guide</title>
+  <title>Relay! Style Guide</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#000000">
 
@@ -25,8 +25,8 @@
 
   <div id="top" class="sg-header" role="banner">
     <div class="sg-container">
-      <h1 class="sg-logo">
-          <em>WOWWAY</em>
+      <h1 class="sg-logo" style="color: white">
+          <em>Relay</em>
         <span class="sg-logo-full">STYLE GUIDE</span>
       </h1>
       <button type="button" class="sg-nav-toggle">Menu</button>
@@ -35,7 +35,7 @@
 
   <div class="sg-wrapper sg-clearfix">
     <div id="nav" class="sg-sidebar" role="navigation">
-      <h2 class="sg-h2 sg-subnav-title">About</h2>
+      <h2 class="sg-h2 sg-subnav-title" style="color: white">About</h2>
       <ul class="sg-nav-group">
         <li>
           <a href="#sg-about">Getting Started</a>
@@ -55,7 +55,7 @@
       <div class="sg-container">
         <div class="sg-info">
           <div class="sg-about sg-section">
-            <h2 id="sg-about" class="sg-h2">Getting Started</h2>
+            <h2 id="sg-about" class="sg-h2" >Getting Started</h2>
             <p>A living style guide is a great tool to promote visual consistency, and speed up development times. </p>
             <p></p>
           </div><!--/.sg-about-->
@@ -65,145 +65,131 @@
                 <h2 id="sg-colors" class="sg-h2">Colors</h2>
                 <div class="sg-color-grid">
                     <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #c62328;"></div>
-                        <div class="sg-color-name">Red</div>
-                        <div class="sg-color-value">#c62328</div>
+                        <div class="sg-color-swatch" style="background-color: #B30838;"></div>
+                        <div class="sg-color-name">Crimson</div>
+                        <div class="sg-color-value">#B30838</div>
                     </div>
                     <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #f26529;"></div>
-                        <div class="sg-color-name">Guava</div>
-                        <div class="sg-color-value">#f26529</div>
+                        <div class="sg-color-swatch" style="background-color: #2C49A4;"></div>
+                        <div class="sg-color-name">Pacific
+                        </div>
+                        <div class="sg-color-value">#2C49A4</div>
                     </div>
                     <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #ffc40c;"></div>
-                        <div class="sg-color-name">Orange</div>
-                        <div class="sg-color-value">#ffc40c</div>
+                        <div class="sg-color-swatch" style="background-color: #A8DADC;"></div>
+                        <div class="sg-color-name">Sea Foam
+                        </div>
+                        <div class="sg-color-value">#A8DADC</div>
                     </div>
                     <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #fff215;"></div>
-                        <div class="sg-color-name">Bumblebee</div>
-                        <div class="sg-color-value">#fff215</div>
+                        <div class="sg-color-swatch" style="background-color: #E63946;"></div>
+                        <div class="sg-color-name">Imperial Red
+                        </div>
+                        <div class="sg-color-value">#E63946</div>
                     </div>
                     <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #10ace3;"></div>
-                        <div class="sg-color-name">Deep Sky Blue</div>
-                        <div class="sg-color-value">#10ace3</div>
+                        <div class="sg-color-swatch" style="background-color: #F7F7F7;"></div>
+                        <div class="sg-color-name">Fog
+                        </div>
+                        <div class="sg-color-value">#F7F7F7</div>
                     </div>
                     <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #0065e3;"></div>
-                        <div class="sg-color-name">Persian Blue</div>
-                        <div class="sg-color-value">#0065e3</div>
+                        <div class="sg-color-swatch" style="background-color: #1D3557;"></div>
+                        <div class="sg-color-name">Prussian
+                        </div>
+                        <div class="sg-color-value">#1D3557</div>
                     </div>
-                </div>
-                <div class="sg-color-grid">
+                    <div class="sg-color">
+                        <div class="sg-color-swatch" style="background-color: #5D97C9;"></div>
+                        <div class="sg-color-name">Sea
+                        </div>
+                        <div class="sg-color-value">#5D97C9</div>
+                    </div>
+                    <div class="sg-color">
+                        <div class="sg-color-swatch" style="background-color: #FFAC5F;"></div>
+                        <div class="sg-color-name">Orange
+                        </div>
+                        <div class="sg-color-value">#FFAC5F</div>
+                    </div>
+                    <div class="sg-color">
+                        <div class="sg-color-swatch" style="background-color: #79B9ED;"></div>
+                        <div class="sg-color-name">Sky Blue
+                        </div>
+                        <div class="sg-color-value">#79B9ED</div>
+                    </div>
                     <div class="sg-color">
                         <div class="sg-color-swatch" style="background-color: #ffffff;"></div>
                         <div class="sg-color-name">White</div>
                         <div class="sg-color-value">#ffffff</div>
                     </div>
                     <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #e5e5ea;"></div>
-                        <div class="sg-color-name">Light Grey</div>
-                        <div class="sg-color-value">#E5E5EA</div>
-                    </div>
-                    <div class="sg-color">
-                        <div class="sg-color-swatch" style="background-color: #58595b;"></div>
-                        <div class="sg-color-name">Grey</div>
-                        <div class="sg-color-value">#58595b</div>
-                    </div>
-                    <div class="sg-color">
                         <div class="sg-color-swatch" style="background-color: #000000;"></div>
                         <div class="sg-color-name">Black</div>
                         <div class="sg-color-value">#000000</div>
                     </div>
-                </div><!--/.sg-color-grid-->
-
+                </div>
+              <!--/.sg-color-grid-->
+            </div>
           <!-- Manually add your fonts here. -->
           <div class="sg-font-stacks sg-section">
             <h2 id="sg-fontStacks" class="sg-h2">Font Stacks</h2>
             <dl class="sg-font-list">
               <dt>Primary Font:</dt>
-              <dd style='font-family: ars-maquette-web, sans-serif;'>Ars Maquette Web, sans-serif</dd>
+              <dd style='font-family: proxima-nova;'>Proxima Nova</dd>
 
               <dt>Primary Font Bold:</dt>
-              <dd style='font-family: ars-maquette-web, sans-serif; font-weight: 700;'>Ars Maquette Web, sans-serif</dd>
+              <dd style='font-family: proxima-nova; font-weight: 700;'>Proxima Nova</dd>
 
               <dt>Secondary Font:</dt>
-              <dd style='font-family: conduit light, sans-serif;'>Conduit Light, sans-serif</dd>
+              <dd style='font-family: chronicle-display;'>Chronicle Display</dd>
 
               <dt>Secondary Font Bold:</dt>
-              <dd style='font-family: conduit light, sans-serif; font-weight: 700;'>Conduit Light, sans-serif</dd>
+              <dd style='font-family: chronicle-display; font-weight: 700;'>Chronicle Display</dd>
             </dl>
             <div class="sg-markup-controls"><a class="sg-btn--top" href="#top">Back to Top</a></div>
           </div><!--/.sg-font-stacks-->
         </div><!--/.sg-info-->
           <!-- Headings -->
-          <u><h1>Headings Style: Hero #SinglePriceTwoButtons</h1></u>
+          <u><h3 style="color:black;">Headings</h3></u>
           <div>
-              <h1 style="font-family: ars-maquette-web, sans-serif;font-weight: 400;font-size: 45px;">Heading 1 with <small>small text</small> and a <a>link</a></h1>
-              <h2 style="font-family: ars-maquette-web, sans-serif;font-size: 32px;">Heading 2 with <small>small text</small> and a <a>link</a></h2>
-              <h3 style="font-family: ars-maquette-web, sans-serif;font-weight:400; text-transform:uppercase;font-size: 20px;">Heading 3 with <small>small text</small> and a <a>link</a></h3>
-              <h4 style="font-family: ars-maquette-web, sans-serif;">Heading 4 with <small>small text</small> and a <a>link</a></h4>
-              <h5 style="font-family: ars-maquette-web, sans-serif;">Heading 5 with <small>small text</small> and a <a>link</a></h5>
-              <h6 style="font-family: ars-maquette-web, sans-serif;">Heading 6 with <small>small text</small> and a <a>link</a></h6>
-              <p style="font-family:unset;font-weight:200;font-size:11px;">Paragraph with numbers 123 and a price $39.99. </p>
+              <h1>Heading 1</h1>
+              <h2>Heading 2</h2>
+              <h3>Heading 3</h3>
+              <h4>Heading 4</h4>
+              <h5>Heading 5</h5>
+              <h6>Heading 6</h6>
           </div>
           <hr>
           <!-- Headings -->
-          <u><h1>Headings Style: c-offer-title span</h1></u>
-          <div>
-              <h1 style="font-family: ars-maquette-web, sans-serif;font-weight: 400;font-size: 52px;">Heading 1 with <small>small text</small> and a <a>link</a></h1>
-              <h2 style="font-family: ars-maquette-web, sans-serif;font-weight:200; font-size: 20px;">Heading 2 with <small>small text</small> and a <a>link</a></h2>
-              <h3 style="font-family: ars-maquette-web, sans-serif;font-weight:200; font-size: 16px;">Heading 3 with <small>small text</small> and a <a>link</a></h3>
-              <p style="font-family:unset;font-weight:200;font-size:14px;">Paragraph with numbers 123 and a price $39.99. </p>
+          <u><h3 style="color:black;">Headings - With Underline</h3></u>
+          <div style="background-image:url('https://picsum.photos/200/300/?blur'); background-repeat: no-repeat; background-size: 100%; padding: 10px 0px;">
+              <h1 class="color-white underline" style="font-family: chronicle-display; font-style: normal; font-weight: 300; font-size: 90px; line-height: 96px;">Heading 1</h1>
           </div>
           <hr>
           <!-- Headings -->
-          <u><h1>Headings Style: offer card</h1></u>
+          <u><h3 style="color:black;">Headings - Callout Block</h3></u>
           <div>
-              <h1 style="font-family: conduit light, sans-serif;font-weight: 500;font-size: 60px;">Heading 1 with <small>small text</small> and a <a>link</a></h1>
-              <h2 style="font-family: ars-maquette-web, sans-serif;font-weight:700; font-size: 16px;">Heading 2 with <small>small text</small> and a <a>link</a></h2>
-              <h3 style="font-family: ars-maquette-web, sans-serif;font-weight:200; font-size: 14px;">Heading 2 with <small>small text</small> and a <a>link</a></h3>
-              <h5 style="font-family: ars-maquette-web, sans-serif;font-weight:700; font-size: 13px;">Heading 2 with <small>small text</small> and a <a>link</a></h5>
-              <p style="font-family:ars-maquette-web, sans-serif;font-weight:100;font-size:10px;">Paragraph with numbers 123 and a price $39.99. </p>
+              <h2 style="font-family: chronicle-display; font-style: normal; font-weight: 300; font-size: 90px; line-height: 96px;">Heading 2</h2>
           </div>
           <hr>
           <!-- Headings -->
-          <u><h1>Headings Style: Simple Paragraph with Title</h1></u>
+          <u><h3 style="color:black;">Headings - Basic block in two columns</h3></u>
           <div>
-              <h2 style="font-size: 48px; font-weight:300;font-family: ars-maquette-web, sans-serif;">About WOW</h2>
-              <p style="font-size:14px; font-weight: 200;font-family: ars-maquette-web, sans-serif;">Some paragraph text</p>
+              <h2 style="font-family: chronicle-display; font-style: normal; font-weight: 300; font-size: 60px; line-height: 68px;">Heading 2</h2>
           </div>
           <hr>
           <!-- Headings -->
-          <u><h1>Headings Style: Hero More Content</h1></u>
-          <div>
-              <h2 style="font-size: 32px; font-family: ars-maquette-web, sans-serif;">About WOW</h2>
-              <p style="font-size:14px; font-weight: 200;font-family: ars-maquette-web, sans-serif;">Some paragraph text with <strong>strong text</strong></p>
+          <u><h3 style="color:black;">Headings - Card title Slider</h3></u>
+          <div class="bg-pacific">
+              <h2 class="color-white"style="font-family: chronicle-display; font-style: normal; font-weight: 300; font-size: 60px; line-height: 68px;">Heading 2</h2>
           </div>
           <hr>
-          <!-- Headings -->
-          <u><h1>Headings Style: Internet Bundles</h1></u>
-          <div>
-              <h3 style="font-size: 18.72px; font-weight:300;font-family: ars-maquette-web, sans-serif;">About WOW</h3>
-              <p style="font-size:.9em; font-weight: 200;font-family: ars-maquette-web, sans-serif;">Some paragraph text with <label style="font-weight: 600; font-size: .7em; text-transform: uppercase;">a label</label> and <span style="font-size: 8px; font-weight: 500;">span text</span></p>
+          <u><h3 style="color:black;">Headings - Three Columns Cards</h3></u>
+          <div class="bg-sea-foam">
+              <h2 class="color-prussian" style="font-family: chronicle-display; font-style: normal; font-weight: 300; font-size: 90px; line-height: 96px;;">Heading 2</h2>
           </div>
           <hr>
-          <!-- Headings -->
-          <u><h1>Headings Style: sf Content Block</h1></u>
-          <div>
-              <span style="font-size: 16px; text-transform: uppercase; color: #a5a5a5;font-family: ars-maquette-web, sans-serif;"></span>
-              <h2 style="font-size: 32px; font-weight:300;font-family: ars-maquette-web, sans-serif;">Heading 2: About WOW</h2>
-              <p style="font-weight: 200;font-family: ars-maquette-web, sans-serif;">Some paragraph text</p>
-          </div>
-          <hr>
-          <!-- Headings -->
-          <u><h1>Headings Style: the Wow experience</h1></u>
-          <div>
-              <h3 style="font-size: 18.72px; font-family: ars-maquette-web, sans-serif;">Heading 3 text with <label style="font-weight: 600;text-transform: uppercase;">label text</label></h3>
-              <p style="font-weight: 200;font-family: ars-maquette-web, sans-serif;">Some paragraph text</p>
-          </div>
-          <hr>
+
 
         <?php renderFilesInFolder('markup'); ?>
 
