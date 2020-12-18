d6c055ea3f45e19c5eff1462ca8f6933d0051da9
fmitchell <fredric@brightplum.com>
2020-12-17 20:33:21 -0600

    news and stories, program tabs, article image header, article stack

--- js/custom.js
+++ js/custom.js
@@ -41,7 +41,6 @@
 
 
     //Card-slider
-
     $('.card-slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -51,9 +50,7 @@
         nextArrow: $('.slider-controls--right')
     });
 
-
     // Plain slider
-
     $('.plain-slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -62,6 +59,52 @@
         prevArrow: $('.plain-left'),
         nextArrow: $('.plain-right'),
         dots: true,
+    });
+
+    // text Slider
+
+    $('.text-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+
+    // Carousel
+
+    $('.carousel-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        centerPadding: '60px',
+        centerMode: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '60px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 
@@ -87,6 +130,37 @@
     $('.main-nav--icon').click(function() {
         $('.parent-menu').toggleClass('open');
         $('.nav').toggleClass('open');
+    });
+
+
+    // Program tabs
+    const select = document.querySelectorAll('.selectBtn');
+    const option = document.querySelectorAll('.option');
+    let index = 1;
+
+    select.forEach(a => {
+        a.addEventListener('click', b => {
+            const next = b.target.nextElementSibling;
+            next.classList.toggle('toggle');
+            next.style.zIndex = index++;
+        });
+    });
+    option.forEach(a => {
+        a.addEventListener('click', b => {
+            b.target.parentElement.classList.remove('toggle');
+
+            const parent = b.target.closest('.select').children[0];
+            parent.setAttribute('data-type', b.target.getAttribute('data-type'));
+            parent.innerText = b.target.innerText;
+        });
+    });
+
+    $('.program-tabs--headers-head').click(function () {
+        let target = $(this).data('target');
+        $('.program-tabs--headers-head').removeClass('active');
+        $(this).addClass('active');
+        $('.program-tabs-tab').removeClass('active');
+        $('#' + target).addClass('active');
     });
 
 
